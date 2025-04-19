339a3887b9fd1ea803a6915c8a7b1dae5555ee50
A-Lee92 <117328193+A-Lee92@users.noreply.github.com>
2025-04-19 02:37:21 -0400

    Updated classify()
    Trained new model, Bigger Badder and Uncut.
    Made prediction labels and true labels and updated messages to show the user what our model is predicting.

--- PlantDiseaseClassifier/PlantDiseaseClassifier/views.py
+++ PlantDiseaseClassifier/PlantDiseaseClassifier/views.py
@@ -2,6 +2,7 @@
 Routes and views for the flask application.
 """
 import os
+import re
 from datetime import datetime
 from flask import render_template, url_for, request
 from PlantDiseaseClassifier import app
@@ -62,36 +63,60 @@ def classify():
     img_size = (224,224)
     test_image_path = request.form['selected_image']
     image_path = os.path.join(app.root_path, test_image_path)
-    parent_dir = os.path.dirname(app.root_path)
-    model_path = os.path.join(app.root_path,'static' , 'model', 'plant_model_test_conv_output_3.keras')
+    
+    parent_dir = os.path.dirname(app.root_path) 
+    model_path = os.path.join(app.root_path,'static' , 'model', 'plant_model_0.keras')
     image_folder = os.path.join(app.root_path, 'static', 'test_images')
-    class_labels = [num for num in range(0, 37)]
-  
+    label_path = os.path.join(app.root_path, 'static', 'class_labels', "classes.txt")
+    
+    class_labels = load_labels(label_path)
     img = load_img(image_path, target_size=img_size)
     img_array = img_to_array(img)
     normalized_img = img_array/255.0
-
+    
+    truth = test_image_path.split('\\')[-1]
+    truth = truth.split('.')[0]
+    if truth[-1].isdigit():
+        truth = truth[:-1]
+    truth = re.split(r'(?=[A-Z])', truth)
+    
+    
     model = load_model(model_path)
     predictions = model.predict(np.expand_dims(normalized_img, axis=0))
     predicted_index = np.argmax(predictions[0])
     predicted_label = class_labels[predicted_index]
-
+    predicted_plant = predicted_label[0]
+    predicted_disease = predicted_label[1]
+    
+    prob = (predictions[0][predicted_index])
+    
+    grammar = "has"
+    if predicted_disease == "healthy":
+        grammar = "is"
+    
+    message_2 = f"The test image above is of a {truth[1]} leaf that {grammar} {' '.join(truth[2:])} "
+    
    # prediction = model.predict(normalized_img.flow_from_directory())
     return render_template(
         'classified.html',
         title='Classfied',
         prediction=predicted_label,
         year=datetime.now().year,
-        message=f'Your plant is: {predicted_label}',
+        message=f'Our model is {prob:.2%} certain that this {predicted_plant} leaf {grammar} {predicted_disease}',
+        message_2 = message_2,
         test_image=test_image_path
     )
 
-def get_predicted_label(index):
-    match index:
-        case i if 0 <= i <= 3: 
-            label = 'Apple Cedar Rust'
-    return list(gen.class_indices.keys())
-
-def get_class_label():
-    labels = [ ]
+def load_labels(path):
+    labels = []
+    with open(path, encoding="utf-8") as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+            
+            plant, disease = line.split("___", 1)
+            plant = plant.replace("_", " ").strip()
+            disease = disease.replace("_", " ").strip()
+            labels.append((plant, disease))
     return labels
